92f06dea0a832b344cc950c63597f6dd55d0782e
Ehab-Ma <mansourehab34@gmail.com>
2025-10-27 19:40:56 +0200

    feat(api): convert vehicle/history serverless functions to ESM default handlers

--- web/api/history/[plate].js
+++ web/api/history/[plate].js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res){
   const { plate } = req.query;
   if (!plate) return res.status(400).json({ error: 'missing plate' });
   res.json({ route: 'history', plate });
